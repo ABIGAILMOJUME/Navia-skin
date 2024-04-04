e268a05cecae3e6653d25432ac37b436cf6b8e20
ABIGAIL MOJUME <102516144+ABIGAILMOJUME@users.noreply.github.com>
2024-04-04 10:24:01 +0100

    colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,19 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        'pink-one': '#F3E8F3',
+        'pink-two': '#E7C9C9',
+        'pink-three':'#D4A7A7',  
+        'pink-four': '##F3E8E8',
+        'grey-one': '#FCFBFA',
+        'grey-two':'#2F2F2F',
+         },
+      fontFamily:{
+          textone :['"Urbanist", sans-serif;']
+         }
+    },
   },
   plugins: [],
 }
