005511801588bd97162d7ad6eac92174f386e734
ABIGAIL MOJUME <102516144+ABIGAILMOJUME@users.noreply.github.com>
2024-04-10 20:38:11 +0100

    new image added

--- src/Components/servicesmenu.jsx
+++ src/Components/servicesmenu.jsx
@@ -2,7 +2,7 @@ const ServiceMenu = () => {
     return ( 
        <>
              <div className="relative flex overflow-x-hidden">
-  <div className=" animate-marquee whitespace-nowrap tracking-wide">
+  <div className="animate-marquee whitespace-nowrap tracking-wide opacity-50">
     <span className="mx-4 text-marquee text-pink-four font-urbanist">UNLEASH YOUR INNER GLOW</span>
     <span className="mx-4 text-marquee text-pink-four font-urbanist">UNLEASH YOUR INNER GLOW</span>
     <span className="mx-4 text-marquee text-pink-four font-urbanist">UNLEASH YOUR INNER GLOW</span>
@@ -16,7 +16,17 @@ const ServiceMenu = () => {
   </div>
 </div>    
         <div>
-            
+            <div className="grid md:grid-cols-2">
+                    <div className="flex">
+                        <div className="bg-pink-three">
+                            OUR SERVICES MENU
+                        </div>
+                    </div>
+
+                    <div>
+
+                    </div>
+            </div>
         </div>
      </>
      );
