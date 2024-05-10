17e15785737669180b948084aebfee3c7ac12fca
ABIGAIL MOJUME <102516144+ABIGAILMOJUME@users.noreply.github.com>
2024-05-10 12:23:53 +0100

    servicemenu done

--- src/Components/Servicesmenu.jsx
+++ src/Components/Servicesmenu.jsx
@@ -15,7 +15,7 @@ const Servicesmenu = () => {
 
         <div className='flex flex-col items-end mx-auto'>
             <h4 className='text-one text-center border-b border-pink-one py-6'>LETS ENHANCE YOUR NATURAL BEAUTY </h4>
-            <h4 className='flex transition ease-in-out delay-500 font-urbanist text-center md:text-left text-4xl sm:leading-8 md:leading-10 text-grey-two hover:text-pink-three py-10 border-b border-pink-one'>Facial Treatments <a href=""></a><FaPlus className='ml-14 pb-2 sm:mt-2 fill-pink-three'/></h4>
+            <h4 className='flex transition ease-in-out delay-500 font-urbanist text-center md:text-left text-4xl sm:leading-8 md:leading-10 text-grey-two hover:text-pink-three py-10 border-b border-pink-one'>Facial Treatments <a href=""></a><FaPlus className='ml-14 pb-2 sm:mt-5 fill-pink-three'/></h4>
             <h4 className='flex transition ease-in-out delay-500 font-urbanist text-center md:text-left text-4xl sm:leading-8 md:leading-10 text-grey-two hover:text-pink-three py-10 border-b border-pink-one'>Chemical Peels <a href=""></a><FaPlus className='ml-24 pb-2  fill-pink-three' /></h4>
             <h4 className='flex transition ease-in-out delay-500 font-urbanist text-center md:text-left text-4xl sm:leading-8 md:leading-10 text-grey-two hover:text-pink-three py-10'>Body Treatments <a href=""></a><FaPlus className='ml-16 pb-2 fill-pink-three'/></h4>
 
